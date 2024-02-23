687fe70a60910c34886788237f55e93eb89eca9b
nicolai jensen <nbje63269@edu.ucl.dk>
2024-02-23 10:59:59 +0100

    Add project files.

---
+++ det rigtige yatzy/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace det_rigtige_yatzy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
