eae94a8fa0a37c26d30a4380039137b3d1817da4
Oliver Højberg Holm <ohho62380@edu.ucl.dk>
2024-02-23 11:06:11 +0100

    Lavet kode til at holde styr på spillere

--- det rigtige yatzy/Program.cs
+++ det rigtige yatzy/Program.cs
@@ -4,7 +4,48 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Start besked
+            Console.WriteLine("TEAM 13 Yahtzee!");
+
+            // Brugerinput (Antal a spillere)
+            Console.WriteLine("Hvor mange skal spille?");
+            int numPlayers = int.Parse(Console.ReadLine());
+
+            // Liste med spillernavne
+            List<Player> players = new List<Player>();
+
+            // Loop for at få spillernes navne
+            for (int i = 0; i < numPlayers; i++)
+            {
+                // Indtast spillernavne
+                Console.WriteLine($"Indtast spillernavn {i + 1}:");
+                string playerName = Console.ReadLine();
+
+                // Gør det nye spillernavn til et opjekt
+                players.Add(new Player(playerName));
+            }
+
+
+        }
+    }
+
+    internal class Player
+    {
+        private string? playerName;
+
+        public Player(string? playerName)
+        {
+            this.playerName = playerName;
+        }
+    }
+
+    internal class YahtzeeGame
+    {
+        private List<Player> players;
+
+        public YahtzeeGame(List<Player> players)
+        {
+            this.players = players;
         }
     }
 }
