2eac1a0d3bf4928b1c1585299819d8adf96caec3
nicolai jensen <nbje63269@edu.ucl.dk>
2024-02-26 08:57:41 +0100

    fået spillernavn og scoreboardet til at passe sammen, og en goto start til hvis de vælger mere end 4 spillere.

--- det rigtige yatzy/Program.cs
+++ det rigtige yatzy/Program.cs
@@ -8,12 +8,22 @@
             Console.WriteLine("Velkommen til TEAM 13 Yahtzy");
             Console.WriteLine("");
 
+            //goto
+            repeat:
+
             // Anmodning om antal spillere
             Console.WriteLine("Indtast antal spillere (2-4)");
 
             // Læs antallet af spillere fra brugeren
             String antal = Console.ReadLine();
             int valgtetal = int.Parse(antal);
+
+            //spillernavne
+            String spiller1 = "";
+            String spiller2 = "";
+            String spiller3 = "";
+            String spiller4 = "";
+
 
             // Switch-sætning baseret på antal spillere
             switch (valgtetal)
@@ -25,75 +35,64 @@
                 case 2:
                     // Anmodning om navne på to spillere, hvis to spillere er valgt
                     Console.WriteLine("Indtast navn på spiller 1:");
-                    string spil1 = Console.ReadLine();
+                    spiller1 = Console.ReadLine();
                     Console.WriteLine("Indtast navn på spiller 2:");
-                    string spil2 = Console.ReadLine();
+                    spiller2 = Console.ReadLine();
                     break;
                 case 3:
                     // Anmodning om navne på tre spillere, hvis tre spillere er valgt
                     Console.WriteLine("Indtast navn på spiller 1:");
-                    string spil31 = Console.ReadLine();
+                    spiller1 = Console.ReadLine();
                     Console.WriteLine("Indtast navn på spiller 2:");
-                    string spil32 = Console.ReadLine();
+                    spiller2 = Console.ReadLine();
                     Console.WriteLine("Indtast navn på spiller 3:");
-                    string spil33 = Console.ReadLine();
+                    spiller3 = Console.ReadLine();
                     break;
                 case 4:
                     // Anmodning om navne på fire spillere, hvis fire spillere er valgt
                     Console.WriteLine("Indtast navn på spiller 1:");
-                    string spil41 = Console.ReadLine();
+                    spiller1 = Console.ReadLine();
                     Console.WriteLine("Indtast navn på spiller 2:");
-                    string spil42 = Console.ReadLine();
+                    spiller2 = Console.ReadLine();
                     Console.WriteLine("Indtast navn på spiller 3:");
-                    string spil43 = Console.ReadLine();
+                    spiller3 = Console.ReadLine();
                     Console.WriteLine("Indtast navn på spiller 4:");
-                    string spil44 = Console.ReadLine();
+                    spiller4 = Console.ReadLine();
                     break;
+
+                    default: Console.WriteLine("Du en Skovl! Prøv igen :D ");
+                    goto repeat;
+                    
 
             }
 
-            // Vent på brugerens input, før du afslutter programmet
-            Console.ReadLine();
-        }
-    
-
-
-
-
-
-// scoreboard 1. udkast prøv at kør det for sig selv, og se om det er den retning det skal være
-Console.WriteLine("skriv navn:");
-            string playername1 = Console.ReadLine();
-            string playername2 = Console.ReadLine();
-            string playername3 = Console.ReadLine();
-            string playername4 = Console.ReadLine();
-
+            // scoreboard 1. udkast 
             Console.Clear();
 
-            int ettere = 4;
-            int toere = 2;
-            int treer = 9;
-            int firer = 16;
-            int femmere = 15;
-            int seksere = 18;
-            int treafenslags = 6;
-            int fireafenslags = 12;
+            int ettere = 0;
+            int toere = 0;
+            int treer = 0;
+            int firer = 0;
+            int femmere = 0;
+            int seksere = 0;
+            int treafenslags = 0;
+            int fireafenslags = 0;
             int hus = 0;
             int denlille = 0;
             int denstore = 0;
             int chance = 0;
             int yatzy = 0;
-            int t1 = 3;
-            int t2 = 4;
-            int t3 = 2;
-            int t4 = 6;
-            int t5 = 2;
+            int t1 = 0;
+            int t2 = 0;
+            int t3 = 0;
+            int t4 = 0;
+            int t5 = 0;
             int antalslag = 3;
 
 
             //Scoreboardet
-            Console.WriteLine($"Y A H T Z E E \t\t\t\t\t\t\t\t Players: \t  1:, 2, 3, 4");
-            Console.WriteLine($"Player 1:{playername1}    Player 2:{playername2}    Player 3:{playername3}    Player 4:{playername4}");
+            Console.WriteLine($"Y A H T Z E E \t\t\t\t\t\t\t\t Players: \t  1:, 2:, 3:, 4:");
+            Console.WriteLine($"Player 1:{spiller1}    Player 2:{spiller2}    Player 3:{spiller3}    Player 4:{spiller4}");
             Console.WriteLine($"\t\t\t\t\t\t\t\t\t 1----------------{ettere}");
             Console.WriteLine($"\t\t\t\t\t\t\t\t\t 2----------------{toere}");
             Console.WriteLine($"\t\t\t\t\t\t\t\t\t 3----------------{treer}");
@@ -107,127 +106,123 @@ Console.WriteLine("skriv navn:");
             Console.WriteLine($"\t\t\t\t\t\t\t\t\t denstore---------{denstore}");
             Console.WriteLine($"\t\t\t\t\t\t\t\t\t chance-----------{chance}");
             Console.WriteLine($"\t\t\t\t\t\t\t\t\t yatzy------------{yatzy}");
-            Console.WriteLine($"{playername1}, tur");
+            Console.WriteLine($"{spiller1}, tur");
             Console.WriteLine($"Dit terningslag {t1}, {t2}, {t3}, {t4}, {t5}\t\t\t slag tilbage:{antalslag}");
-
         }
-    }
+    }   
+}
 
 
-//antal spillere med navn
 
-//scoreboard ses hele tiden
+static int Terning1, Terning2, Terning3, Terning4, Terning5;
+static bool IsTerning1Locked, IsTerning2Locked, IsTerning3Locked, IsTerning4Locked, IsTerning5Locked;
+static int Kast;
 
-//terningen
+// Tjekker om vores terning er locked og vores Kast som senere max er 3
+static void Main(string[] args)
 {
-    static int Terning1, Terning2, Terning3, Terning4, Terning5;
-    static bool IsTerning1Locked, IsTerning2Locked, IsTerning3Locked, IsTerning4Locked, IsTerning5Locked;
-    static int Kast;
+    IsTerning1Locked = false;
+    IsTerning2Locked = false;
+    IsTerning3Locked = false;
+    IsTerning4Locked = false;
+    IsTerning5Locked = false;
+    Kast = 0;
 
-    // Tjekker om vores terning er locked og vores Kast som senere max er 3
-    static void Main(string[] args)
+    while (Kast < 3)
     {
-        IsTerning1Locked = false;
-        IsTerning2Locked = false;
-        IsTerning3Locked = false;
-        IsTerning4Locked = false;
-        IsTerning5Locked = false;
-        Kast = 0;
-
-        while (Kast < 3)
+        if (Kast == 0)
         {
-            if (Kast == 0)
-            {
-                Console.WriteLine("Skriv kast for at slå\n");
-            }
-            else
-            {
-                // Sp'rg om du vil l[se din terning eller kaste igen
-                Console.WriteLine("Skriv Kast for at slå med terningerne igen eller skriv lås tallet på den terning du gerne vil låse 1-5\n");
-            }
-
-            // Man kan også bruge ''if (Console.ReadKey().Key == ConsoleKey.S)'' Hvis man bare vil bruge en knap istedet for at skrive noget
-            string userInput = Console.ReadLine();
-            if (userInput == "Kast" || userInput == "kast")
-            {
-                KastTerninger();
-
-                // Skriver terningernes tal ud
-                Console.WriteLine($"Kast {Kast}: {Terning1} {Terning2} {Terning3} {Terning4} {Terning5}\n"); // Viser at terningen er låst
-
-                Console.WriteLine($"Du har brugt {Kast} slag\n");
-            }
-            else if (userInput == "Lås" || userInput == "lås")
-            {
-                // Spørg om du vil låse din terning eller kaste igen
-                Console.WriteLine("Skriv tallet på den terning du gerne vil låse [1-5]:\n");
-                int terningNummer = int.Parse(Console.ReadLine());
-                LåsTerning(terningNummer);
-            }
-        }
-    }
-
-    private static void KastTerninger()
-    {
-        Random r = new Random();
-
-        //TO-DO tjek om terningen er låst inden den får en ny værdi
-        if (!IsTerning1Locked)
-        {
-            Terning1 = r.Next(1, 7);
-        }
-        if (!IsTerning2Locked)
-        {
-            Terning2 = r.Next(1, 7);
-        }
-        if (!IsTerning3Locked)
-        {
-            Terning3 = r.Next(1, 7);
-        }
-        if (!IsTerning4Locked)
-        {
-            Terning4 = r.Next(1, 7);
-        }
-        if (!IsTerning5Locked)
-        {
-            Terning5 = r.Next(1, 7);
-        }
-
-        Kast++;
-    }
-
-    private static void LåsTerning(int DiceNumber) //TO-DO Lås terningen
-    {
-        if (DiceNumber == 1)
-        {
-            IsTerning1Locked = true;
-        }
-        else if (DiceNumber == 2)
-        {
-            IsTerning2Locked = true;
-        }
-        else if (DiceNumber == 3)
-        {
-            IsTerning3Locked = true;
-        }
-        else if (DiceNumber == 4)
-        {
-            IsTerning4Locked = true;
-        }
-        else if (DiceNumber == 5)
-        {
-            IsTerning5Locked = true;
+            Console.WriteLine("Skriv kast for at slå\n");
         }
         else
         {
-            Console.WriteLine("\nUgyldigt terningnummer hvorfor prøver du ikke igen lille idiot? :)");
+            // Spørg om du vil låse din terning eller kaste igen
+            Console.WriteLine("Skriv Kast for at slå med terningerne igen eller skriv lås tallet på den terning du gerne vil låse 1-5\n");
+        }
+
+        // Man kan også bruge ''if (Console.ReadKey().Key == ConsoleKey.S)'' Hvis man bare vil bruge en knap istedet for at skrive noget
+        string userInput = Console.ReadLine();
+        if (userInput == "Kast" || userInput == "kast")
+        {
+            KastTerninger();
+
+            // Skriver terningernes tal ud
+            Console.WriteLine($"Kast {Kast}: {Terning1} {Terning2} {Terning3} {Terning4} {Terning5}\n"); // Viser at terningen er låst
+
+            Console.WriteLine($"Du har brugt {Kast} slag\n");
+        }
+        else if (userInput == "Lås" || userInput == "lås")
+        {
+            // Spørg om du vil låse din terning eller kaste igen
+            Console.WriteLine("Skriv tallet på den terning du gerne vil låse [1-5]:\n");
+            int terningNummer = int.Parse(Console.ReadLine());
+            LåsTerning(terningNummer);
         }
     }
 }
 
+private static void KastTerninger()
+{
+    Random r = new Random();
+
+    //TO-DO tjek om terningen er låst inden den får en ny værdi
+    if (!IsTerning1Locked)
+    {
+        Terning1 = r.Next(1, 7);
+    }
+    if (!IsTerning2Locked)
+    {
+        Terning2 = r.Next(1, 7);
+    }
+    if (!IsTerning3Locked)
+    {
+        Terning3 = r.Next(1, 7);
+    }
+    if (!IsTerning4Locked)
+    {
+        Terning4 = r.Next(1, 7);
+    }
+    if (!IsTerning5Locked)
+    {
+        Terning5 = r.Next(1, 7);
+    }
+
+    Kast++;
 }
 
+private static void LåsTerning(int DiceNumber) //TO-DO Lås terningen
+{
+    if (DiceNumber == 1)
+    {
+        IsTerning1Locked = true;
+    }
+    else if (DiceNumber == 2)
+    {
+        IsTerning2Locked = true;
+    }
+    else if (DiceNumber == 3)
+    {
+        IsTerning3Locked = true;
+    }
+    else if (DiceNumber == 4)
+    {
+        IsTerning4Locked = true;
+    }
+    else if (DiceNumber == 5)
+    {
+        IsTerning5Locked = true;
+    }
+    else
+    {
+        Console.WriteLine("\nUgyldigt terningnummer hvorfor prøver du ikke igen lille idiot? :)");
+    }
+}
+
+
+
+
+//antal spillere med navn
+//scoreboard ses hele tiden
+//terningen
 //selve spillet kigge på regler, holde terninger, opdater scoreboard
-
-
 //15 ture afslut, implementer bonuser
