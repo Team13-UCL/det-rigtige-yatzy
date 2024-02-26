50e633fd42fca087e1684807705b357c2e16ee44
Nicolai.Tofteby <nicolaitofteby@gmail.com>
2024-02-26 10:21:43 +0100

    Flyttede lidt rundt på det hele

--- det rigtige yatzy/Program.cs
+++ det rigtige yatzy/Program.cs
@@ -8,8 +8,8 @@
             Console.WriteLine("Velkommen til TEAM 13 Yahtzy");
             Console.WriteLine("");
 
-            //goto
-            repeat:
+        //goto
+        repeat:
 
             // Anmodning om antal spillere
             Console.WriteLine("Indtast antal spillere (2-4)");
@@ -60,11 +60,119 @@
                     spiller4 = Console.ReadLine();
                     break;
 
-                    default: Console.WriteLine("Du en Skovl! Prøv igen :D ");
+                default:
+                    Console.WriteLine("Du en Skovl! Prøv igen :D ");
                     goto repeat;
-                    
+
 
             }
+
+            static int Terning1, Terning2, Terning3, Terning4, Terning5;
+            static bool IsTerning1Locked, IsTerning2Locked, IsTerning3Locked, IsTerning4Locked, IsTerning5Locked;
+            static int Kast;
+
+            // Tjekker om vores terning er locked og vores Kast som senere max er 3
+            static void Main(string[] args)
+            {
+                IsTerning1Locked = false;
+                IsTerning2Locked = false;
+                IsTerning3Locked = false;
+                IsTerning4Locked = false;
+                IsTerning5Locked = false;
+                Kast = 0;
+
+                while (Kast < 3)
+                {
+                    if (Kast == 0)
+                    {
+                        Console.WriteLine("Skriv kast for at slå\n");
+                    }
+                    else
+                    {
+                        // Spørg om du vil låse din terning eller kaste igen
+                        Console.WriteLine("Skriv Kast for at slå med terningerne igen eller skriv lås tallet på den terning du gerne vil låse 1-5\n");
+                    }
+
+                    // Man kan også bruge ''if (Console.ReadKey().Key == ConsoleKey.S)'' Hvis man bare vil bruge en knap istedet for at skrive noget
+                    string userInput = Console.ReadLine();
+                    if (userInput == "Kast" || userInput == "kast")
+                    {
+                        KastTerninger();
+
+                        // Skriver terningernes tal ud
+                        Console.WriteLine($"Kast {Kast}: {Terning1} {Terning2} {Terning3} {Terning4} {Terning5}\n"); // Viser at terningen er låst
+
+                        Console.WriteLine($"Du har brugt {Kast} slag\n");
+                    }
+                    else if (userInput == "Lås" || userInput == "lås")
+                    {
+                        // Spørg om du vil låse din terning eller kaste igen
+                        Console.WriteLine("Skriv tallet på den terning du gerne vil låse [1-5]:\n");
+                        int terningNummer = int.Parse(Console.ReadLine());
+                        LåsTerning(terningNummer);
+                    }
+                }
+            }
+
+            private static void KastTerninger()
+            {
+                Random r = new Random();
+
+                //TO-DO tjek om terningen er låst inden den får en ny værdi
+                if (!IsTerning1Locked)
+                {
+                    Terning1 = r.Next(1, 7);
+                }
+                if (!IsTerning2Locked)
+                {
+                    Terning2 = r.Next(1, 7);
+                }
+                if (!IsTerning3Locked)
+                {
+                    Terning3 = r.Next(1, 7);
+                }
+                if (!IsTerning4Locked)
+                {
+                    Terning4 = r.Next(1, 7);
+                }
+                if (!IsTerning5Locked)
+                {
+                    Terning5 = r.Next(1, 7);
+                }
+
+                Kast++;
+            }
+
+            private static void LåsTerning(int DiceNumber) //TO-DO Lås terningen
+            {
+                if (DiceNumber == 1)
+                {
+                    IsTerning1Locked = true;
+                }
+                else if (DiceNumber == 2)
+                {
+                    IsTerning2Locked = true;
+                }
+                else if (DiceNumber == 3)
+                {
+                    IsTerning3Locked = true;
+                }
+                else if (DiceNumber == 4)
+                {
+                    IsTerning4Locked = true;
+                }
+                else if (DiceNumber == 5)
+                {
+                    IsTerning5Locked = true;
+                }
+                else
+                {
+                    Console.WriteLine("\nUgyldigt terningnummer hvorfor prøver du ikke igen lille idiot? :)");
+                }
+            }
+
+
+
 
             // scoreboard 1. udkast 
             Console.Clear();
@@ -82,12 +190,7 @@
             int denstore = 0;
             int chance = 0;
             int yatzy = 0;
-            int t1 = 0;
-            int t2 = 0;
-            int t3 = 0;
-            int t4 = 0;
-            int t5 = 0;
-            int antalslag = 3;
+            
 
 
             //Scoreboardet
@@ -107,122 +210,15 @@
             Console.WriteLine($"\t\t\t\t\t\t\t\t\t chance-----------{chance}");
             Console.WriteLine($"\t\t\t\t\t\t\t\t\t yatzy------------{yatzy}");
             Console.WriteLine($"{spiller1}, tur");
-            Console.WriteLine($"Dit terningslag {t1}, {t2}, {t3}, {t4}, {t5}\t\t\t slag tilbage:{antalslag}");
-        }
-    }   
-}
-
-
-
-static int Terning1, Terning2, Terning3, Terning4, Terning5;
-static bool IsTerning1Locked, IsTerning2Locked, IsTerning3Locked, IsTerning4Locked, IsTerning5Locked;
-static int Kast;
-
-// Tjekker om vores terning er locked og vores Kast som senere max er 3
-static void Main(string[] args)
-{
-    IsTerning1Locked = false;
-    IsTerning2Locked = false;
-    IsTerning3Locked = false;
-    IsTerning4Locked = false;
-    IsTerning5Locked = false;
-    Kast = 0;
-
-    while (Kast < 3)
-    {
-        if (Kast == 0)
-        {
-            Console.WriteLine("Skriv kast for at slå\n");
-        }
-        else
-        {
-            // Spørg om du vil låse din terning eller kaste igen
-            Console.WriteLine("Skriv Kast for at slå med terningerne igen eller skriv lås tallet på den terning du gerne vil låse 1-5\n");
-        }
-
-        // Man kan også bruge ''if (Console.ReadKey().Key == ConsoleKey.S)'' Hvis man bare vil bruge en knap istedet for at skrive noget
-        string userInput = Console.ReadLine();
-        if (userInput == "Kast" || userInput == "kast")
-        {
-            KastTerninger();
-
-            // Skriver terningernes tal ud
-            Console.WriteLine($"Kast {Kast}: {Terning1} {Terning2} {Terning3} {Terning4} {Terning5}\n"); // Viser at terningen er låst
-
-            Console.WriteLine($"Du har brugt {Kast} slag\n");
-        }
-        else if (userInput == "Lås" || userInput == "lås")
-        {
-            // Spørg om du vil låse din terning eller kaste igen
-            Console.WriteLine("Skriv tallet på den terning du gerne vil låse [1-5]:\n");
-            int terningNummer = int.Parse(Console.ReadLine());
-            LåsTerning(terningNummer);
+            Console.WriteLine($"Dit terningslag {Terning1}, {Terning2}, {Terning3}, {Terning4}, {Terning5}\t\t\t antal kast:{Kast}");
         }
     }
 }
 
-private static void KastTerninger()
-{
-    Random r = new Random();
-
-    //TO-DO tjek om terningen er låst inden den får en ny værdi
-    if (!IsTerning1Locked)
-    {
-        Terning1 = r.Next(1, 7);
-    }
-    if (!IsTerning2Locked)
-    {
-        Terning2 = r.Next(1, 7);
-    }
-    if (!IsTerning3Locked)
-    {
-        Terning3 = r.Next(1, 7);
-    }
-    if (!IsTerning4Locked)
-    {
-        Terning4 = r.Next(1, 7);
-    }
-    if (!IsTerning5Locked)
-    {
-        Terning5 = r.Next(1, 7);
-    }
-
-    Kast++;
-}
-
-private static void LåsTerning(int DiceNumber) //TO-DO Lås terningen
-{
-    if (DiceNumber == 1)
-    {
-        IsTerning1Locked = true;
-    }
-    else if (DiceNumber == 2)
-    {
-        IsTerning2Locked = true;
-    }
-    else if (DiceNumber == 3)
-    {
-        IsTerning3Locked = true;
-    }
-    else if (DiceNumber == 4)
-    {
-        IsTerning4Locked = true;
-    }
-    else if (DiceNumber == 5)
-    {
-        IsTerning5Locked = true;
-    }
-    else
-    {
-        Console.WriteLine("\nUgyldigt terningnummer hvorfor prøver du ikke igen lille idiot? :)");
-    }
-}
-
-
-
-
-//antal spillere med navn
-//scoreboard ses hele tiden
-//terningen
-//selve spillet kigge på regler, holde terninger, opdater scoreboard
+//antal spillere med navn = ✓
+//scoreboard ses hele tiden = ✓
+//terningen = ✓
+//holde terninger = ✓
+//selve spillets gang
+//kigge på reglerne og opdater scoreboard
 //15 ture afslut, implementer bonuser
